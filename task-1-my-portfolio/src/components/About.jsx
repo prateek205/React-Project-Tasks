0b4395c0f918549cfa44d6da32c2b400948f7527
Prateek Bahad <prateekbahad@gmail.com>
2026-03-30 00:19:22 +0530

    update components/About.jsx

---
+++ task-1-my-portfolio/src/components/About.jsx
@@ -0,0 +1,17 @@
+const About = () => {
+  return (
+    <>
+      <section className="p-12 text-center">
+        <h1 className="text-5xl mt-5 underline underline-offset-8 decoration-cyan-500">
+          About <span className="text-cyan-500 font-bold">Me</span>
+        </h1>
+        <p className="py-12">
+          I'm MERN Stack Developer, passionate about building Full Stack Web
+          Application.
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default About;
--- task-1-my-portfolio/src/pages/Home.jsx
+++ task-1-my-portfolio/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import About from "../components/About";
 import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
 
@@ -7,6 +8,7 @@ const Home = () => {
       <div className="flex flex-col bg-black text-white">
         <Navbar />
         <HeroSection />
+        <About/>
       </div>
     </>
   );
