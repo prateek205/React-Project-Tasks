4e195be0fe438ed347be73e652bc1b43de89ca84
Prateek Bahad <prateekbahad@gmail.com>
2026-03-29 21:44:38 +0530

    update the components/Navbar.jsx

--- task-1-my-portfolio/src/App.jsx
+++ task-1-my-portfolio/src/App.jsx
@@ -3,9 +3,9 @@ import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <>
+    <div>
       <Home />
-    </>
+    </div>
   );
 };
 
--- task-1-my-portfolio/src/components/Navbar.jsx
+++ task-1-my-portfolio/src/components/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <section className="h-16 p-2">
+        <nav className="flex items-center justify-between">
+          <h1 className="text-4xl uppercase font-light">
+            My Port<span className="font-bold text-cyan-400">folio</span>
+          </h1>
+          <ul className="flex gap-9 text-lg font-light">
+            <li className="hover:text-cyan-500 hover:underline hover:transition-all cursor-pointer">
+              Home
+            </li>
+            <li className="hover:text-cyan-500 hover:underline hover:transition-all cursor-pointer">
+              About
+            </li>
+            <li className="hover:text-cyan-500 hover:underline hover:transition-all cursor-pointer">
+              Skills
+            </li>
+            <li className="hover:text-cyan-500 hover:underline hover:transition-all cursor-pointer">
+              Projects
+            </li>
+            <li className="hover:text-cyan-500 hover:underline hover:transition-all cursor-pointer">
+              Contact
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
