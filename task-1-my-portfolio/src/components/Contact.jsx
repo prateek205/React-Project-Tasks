67db9c664ff5db7d18923e90790899954f03ebf4
Prateek Bahad <prateekbahad@gmail.com>
2026-03-30 23:09:20 +0530

    update components/contact.jsx

--- task-1-my-portfolio/src/components/Contact.jsx
+++ task-1-my-portfolio/src/components/Contact.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
     <>
-    <div>
-        Contact
-    </div>
+      <section className="h-full flex flex-col items-center bg-gray-600">
+        <div className="py-5">
+          <h2 className="font-light text-4xl underline underline-offset-8 decoration-cyan-500">
+            Contact <span className="font-bold text-cyan-500">Me</span>
+          </h2>
+        </div>
+        <div className="p-10">
+          <p className="text-2xl font-bold">prateekbahad@gmail.com</p>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
