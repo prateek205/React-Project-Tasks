0c5b5617c0cb3fbd9c837802a1fc2e967b5fe14f
Prateek Bahad <prateekbahad@gmail.com>
2026-03-30 11:51:51 +0530

    update components/Skill.jsx

--- task-1-my-portfolio/src/components/Skills.jsx
+++ task-1-my-portfolio/src/components/Skills.jsx
@@ -1,8 +1,36 @@
-
 const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
-}
+  const Skill = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React.js",
+    "Node.js",
+    "Express.js",
+    "MongoDB",
+    "Tailwind-css",
+    "Postman",
+    "Vercel",
+    "GitHub",
+    "Netlify",
+  ];
 
-export default Skills
+  return (
+    <div className="h-full flex flex-col gap-10 items-center mt-5 p-5 bg-gray-600">
+      <h2 className="text-4xl font-light underline underline-offset-8 decoration-cyan-500">
+        My <span className="font-bold text-cyan-500">Skills</span>
+      </h2>
+      <div className="flex gap-5">
+        {Skill.map((item, index) => (
+          <span
+            key={index}
+            className="text-black text-lg px-5 py-2 rounded-md cursor-pointer hover:bg-cyan-300 hover:text-black hover:-translate-y-1 bg-gray-300 transition-all"
+          >
+            {item}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
