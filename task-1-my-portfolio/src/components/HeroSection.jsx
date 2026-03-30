ce73a5d0f72a793920130bcafa36cd2807c8ee5c
Prateek Bahad <prateekbahad@gmail.com>
2026-03-30 10:48:20 +0530

    update components/About.jsx heroSection.jsx

--- task-1-my-portfolio/src/components/HeroSection.jsx
+++ task-1-my-portfolio/src/components/HeroSection.jsx
@@ -1,23 +1,27 @@
+import heroImage from "../assets/heroBanner.png";
 const HeroSection = () => {
   return (
     <>
-      <section className="h-screen flex justify-between px-10 py-5 bg-gray-900 text-white">
-        <div className="flex flex-col basis-1/2 justify-center items-start">
+      <section
+        className="h-screen flex justify-between text-white"
+        style={{ backgroundImage: `url(${heroImage})` }}
+      >
+        <div className="bg-gray-900/70 w-screen px-10 flex flex-col justify-center items-start">
           <h1 className="text-7xl font-bold">Hi, I'm Prateek 👋</h1>
-          <h2 className="text-2xl mt-5 text-cyan-500">MERN Stack Developer</h2>
+          <h2 className="text-3xl mt-5 text-gray-800 font-bold">
+            MERN Stack Developer
+          </h2>
           <p className="text-lg mt-6 ">
             I Build Modern Reponsive Web Application.
           </p>
-          <button className="mt-5 text-lg px-5 py-2 rounded-md bg-cyan-400 text-black cursor-pointer hover:bg-cyan-300 hover:transition-all hover:-translate-y-0.5">
-            View Projects
-          </button>
-        </div>
-        <div className="flex justify-center basis-1/2">
-          <img
-            src="./profileImage.png"
-            alt=""
-            className="w-46 h-46 object-contain"
-          />
+          <div className="flex gap-5 items-center">
+            <button className="mt-5 text-md px-8 py-2 rounded-md bg-cyan-400 text-black cursor-pointer hover:bg-cyan-300 hover:transition-all hover:-translate-y-0.5">
+              View Projects
+            </button>
+            <button className="text-md mt-5 rounded-md px-8 py-2 bg-white text-black cursor-pointer hover:bg-gray-500 hover:text-white hover:transition-all">
+              Download Resume
+            </button>
+          </div>
         </div>
       </section>
     </>
