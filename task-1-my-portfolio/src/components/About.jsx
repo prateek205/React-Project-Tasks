738757a089d5adc4b45c7af2edc2f7f5141d80dd
Prateek Bahad <prateekbahad@gmail.com>
2026-03-30 09:52:00 +0530

    update About.jsx

--- task-1-my-portfolio/src/components/About.jsx
+++ task-1-my-portfolio/src/components/About.jsx
@@ -2,12 +2,20 @@ const About = () => {
   return (
     <>
       <section className="p-12 text-center">
-        <h1 className="text-5xl mt-5 underline underline-offset-8 decoration-cyan-500">
+        <h1 className="text-4xl mt-5 underline underline-offset-8 decoration-cyan-500">
           About <span className="text-cyan-500 font-bold">Me</span>
         </h1>
         <p className="py-12">
-          I'm MERN Stack Developer, passionate about building Full Stack Web
-          Application.
+          I am a passionate Full Stack MERN Developer with experience in
+          building modern and scalable web applications. I specialize in
+          developing responsive user interfaces using React.js and creating
+          backend services using Node.js and Express.I completed my Bachelor of
+          Technology in Mechanical Engineering from Aurangabad College of
+          Engineering with a CGPA of 8.20. During my academic journey, I
+          developed a strong interest in software development and started
+          focusing on web technologies. I enjoy solving problems, building clean
+          user interfaces, and developing full stack applications using the MERN
+          stack.
         </p>
       </section>
     </>
