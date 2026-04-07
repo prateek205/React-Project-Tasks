9daa87b778aa357b63be2ffd2f9d8803f6d1b06c
Prateek Bahad <prateekbahad@gmail.com>
2026-04-07 12:28:25 +0530

    update task

--- task-2-props-destructing/src/components/ImageCard.jsx
+++ task-2-props-destructing/src/components/ImageCard.jsx
@@ -4,9 +4,13 @@ export default function Image({ url, caption }) {
   return (
     <div>
       <h1 className="font-bold">Task - 9 Image Card</h1>
-      <div className="w-56 h-full border border-12 border-black">
-        <img className="w-full object-contain" src={url} alt={caption} />
-        <p className="text-center">{caption}</p>
+      <div className="w-56 h-full border-2 border-black rounded-md">
+        <img
+          className="w-full object-contain rounded-t-sm rounded-b-none"
+          src={url}
+          alt={caption}
+        />
+        <p className="text-center p-2">{caption}</p>
       </div>
     </div>
   );
