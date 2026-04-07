7ab7d3c492b370d6b6029c5856c38677425ffee2
Prateek Bahad <prateekbahad@gmail.com>
2026-04-07 23:33:49 +0530

    update task-3-useState

---
+++ task-3-usestate/src/App.jsx
@@ -0,0 +1,9 @@
+import Toggle from "./components/Toggle";
+
+export default function App() {
+  return (
+    <div className="py-5 px-8">
+      <Toggle/>
+    </div>
+  );
+}
---
+++ task-3-usestate/src/components/Toggle.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function Toggle() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <h1 className="font-bold text-2xl">Task-1 Toggle</h1>
+
+      <div className="w-1/6 flex flex-col gap-3">
+        <button
+          onClick={() => setIsVisible(!isVisible)}
+          className="w-full border-2 border-black py-1 px-3 rounded-md hover:bg-blue-500 hover:text-white"
+        >
+          {!isVisible ? "Hide" : "Show"}
+        </button>
+
+        {!isVisible && <p className="font-bold text-lg">React is Awesome!</p>}
+      </div>
+    </div>
+  );
+}
